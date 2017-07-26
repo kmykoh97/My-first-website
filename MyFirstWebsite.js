1db298b08dd59be3b38d9f95f776f6761ea9c8d5
KOH MENG YIT <kmykoh97@gmail.com>
2017-07-26 16:14:37 +0800

    Update MyFirstWebsite.js

--- MyFirstWebsite.js
+++ MyFirstWebsite.js
@@ -1,9 +1,13 @@
 // JavaScript source code
 
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
 }
