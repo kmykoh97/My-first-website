eda50b5688930faad403638d237dcfd55194f4cb
KOH MENG YIT <kmykoh97@gmail.com>
2017-07-26 16:43:59 +0800

    Update MyFirstWebsite.js
    change zoom value

--- MyFirstWebsite.js
+++ MyFirstWebsite.js
@@ -3,7 +3,7 @@
 function initMap() {
   var uluru = {lat: 3.0243783, lng: 101.74252999999999};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
+    zoom: 12,
     center: uluru
   });
   var marker = new google.maps.Marker({
